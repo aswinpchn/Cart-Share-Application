92ae6ce64239c1f24c24352b76a314a179a786dc
Vidita <viditavijaykumar.daga@sjsu.edu>
2020-04-15 16:01:08 -0700

    Removing code

--- src/main/java/edu/sjsu/cmpe275/cartpool/aspect/UserAspect.java
+++
@@ -1,5 +0,0 @@
-package edu.sjsu.cmpe275.cartpool.aspect;
-
-public class UserAspect {
-
-}
--- src/main/java/edu/sjsu/cmpe275/cartpool/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package edu.sjsu.cmpe275.cartpool.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import edu.sjsu.cmpe275.cartpool.dto.User;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-
-}
