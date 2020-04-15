79ad742975e149789d2f7f7656302a2727dfcbf2
Vidita <viditavijaykumar.daga@sjsu.edu>
2020-04-15 16:01:08 -0700

    Removing code

--- src/main/java/edu/sjsu/cmpe275/cartpool/service/UserService.java
+++
@@ -1,9 +0,0 @@
-package edu.sjsu.cmpe275.cartpool.service;
-
-import edu.sjsu.cmpe275.cartpool.repository.UserRepository;
-
-public class UserService {
-	
-	private UserRepository userRepository;
-
-}
