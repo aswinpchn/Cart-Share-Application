3fe5e80d5f7cbd96a93605c1d64b28825ca8918d
Vidita <viditavijaykumar.daga@sjsu.edu>
2020-04-16 16:04:27 -0700

    Ignoring DS_store

---
+++ backend/src/main/java/edu/sjsu/cmpe275/cartpool/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package edu.sjsu.cmpe275.cartpool.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import edu.sjsu.cmpe275.cartpool.dto.User;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+}
