69bf5a51422c53e1422b994f65fc74e19c3f6be1
Vidita <viditavijaykumar.daga@sjsu.edu>
2020-04-20 21:52:00 -0700

    Integrated get stores and get products backend and frontend

--- backend/src/main/java/edu/sjsu/cmpe275/cartpool/config/WebConfig.java
+++ backend/src/main/java/edu/sjsu/cmpe275/cartpool/config/WebConfig.java
@@ -3,6 +3,7 @@ package edu.sjsu.cmpe275.cartpool.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
 import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -21,5 +22,15 @@ public class WebConfig implements WebMvcConfigurer {
 				mediaType("xml", MediaType.APPLICATION_XML).
 				mediaType("XML", MediaType.APPLICATION_XML).
 				mediaType("json", MediaType.APPLICATION_JSON);
+	}
+	
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/cartpool/**")
+		.allowedOrigins("http://localhost:3000")
+		.allowedMethods("PUT", "DELETE", "GET", "POST")
+		//.allowedHeaders("header1", "header2", "header3")
+		//.exposedHeaders("header1", "header2")
+		.allowCredentials(true).maxAge(3600);
 	}
 }
