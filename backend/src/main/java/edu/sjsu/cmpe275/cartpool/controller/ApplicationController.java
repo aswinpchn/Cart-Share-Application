8206d17cffc4ea5ca0450d6eb8cc38b82702f8eb
Aswin Prasad <aswinpchn@gmail.com>
2020-04-21 23:13:06 -0700

    User: Create user.

--- backend/src/main/java/edu/sjsu/cmpe275/cartpool/controller/ApplicationController.java
+++ backend/src/main/java/edu/sjsu/cmpe275/cartpool/controller/ApplicationController.java
@@ -2,17 +2,16 @@ package edu.sjsu.cmpe275.cartpool.controller;
 
 import java.util.List;
 
+import edu.sjsu.cmpe275.cartpool.dto.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
-import edu.sjsu.cmpe275.cartpool.dto.Product;
-import edu.sjsu.cmpe275.cartpool.dto.Store;
-import edu.sjsu.cmpe275.cartpool.dto.User;
-import edu.sjsu.cmpe275.cartpool.dto.Address;
 import edu.sjsu.cmpe275.cartpool.service.ProductService;
 import edu.sjsu.cmpe275.cartpool.service.StoreService;
 import edu.sjsu.cmpe275.cartpool.service.UserService;
+
+import javax.validation.Valid;
 
 @Controller
 @RequestMapping(path = "/cartpool")
@@ -41,25 +40,26 @@ public class ApplicationController {
     }
     @PostMapping("/user/register")
     @ResponseBody
-    public void createUser(@RequestBody User userBody) {
-        System.out.println("in api"+ userBody);
-//        User user = new User();
-//        Address address = new Address();
-//        if(uid != null) {
-//            user.setUid(uid);
-//        } else {
-//            user.setPassword(password);
-//        }
-//        user.setEmail(email);
-//        user.setScreenName(screenName);
-//        user.setNickName(nickName);
-//        user.setCreditScore(0);
-//        user.setVerified(false);
-//        address.setStreet(street);
-//        address.setCity(city);
-//        address.setState(state);
-//        address.setZip(zip);
-//        user.setAddress(address);
-//        return userService.createUser(user);
+    public User createUser(@Valid @RequestBody CreateUserRequestBodyModel createUserRequestBody) {
+        System.out.println("in api:    "+ createUserRequestBody);
+        User user = new User();
+        Address address = new Address();
+        if(createUserRequestBody.getUid() != null) {
+            user.setUid(createUserRequestBody.getUid());
+        } else {
+            user.setPassword(createUserRequestBody.getPassword());
+        }
+        user.setEmail(createUserRequestBody.getEmail());
+        user.setScreenName(createUserRequestBody.getScreenName());
+        user.setNickName(createUserRequestBody.getNickName());
+        user.setCreditScore(0);
+        user.setVerified(false);
+        address.setStreet(createUserRequestBody.getStreet());
+        address.setCity(createUserRequestBody.getCity());
+        address.setState(createUserRequestBody.getState());
+        address.setZip(createUserRequestBody.getZip());
+        user.setAddress(address);
+        
+        return userService.createUser(user);
     }
 }
