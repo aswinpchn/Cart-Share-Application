c748e3b065a6f0a00c80ac56a19a196f8b1cbe9f
Vidita <viditavijaykumar.daga@sjsu.edu>
2020-04-22 18:41:26 -0700

    Implementing set user API

--- backend/src/main/java/edu/sjsu/cmpe275/cartpool/controller/ApplicationController.java
+++ backend/src/main/java/edu/sjsu/cmpe275/cartpool/controller/ApplicationController.java
@@ -18,55 +18,71 @@ import javax.validation.Valid;
 @CrossOrigin(origins = "http://localhost:3000")
 public class ApplicationController {
 
-    @Autowired
-    private UserService userService;
+	@Autowired
+	private UserService userService;
 
-    @Autowired
-    private StoreService storeService;
+	@Autowired
+	private StoreService storeService;
 
-    @Autowired
-    private ProductService productService;
+	@Autowired
+	private ProductService productService;
 
-    @GetMapping("/store/all")
-    @ResponseBody
-    public List<Store> getAllStores() {
-        return storeService.getAllStores();
-    }
+	@GetMapping("/store/all")
+	@ResponseBody
+	public List<Store> getAllStores() {
+		return storeService.getAllStores();
+	}
 
-    @GetMapping("/product/{storeId}")
-    @ResponseBody
-    public List<Product> getProducts(@PathVariable("storeId") long storeId) {
-        return productService.getProductsByStore(storeId);
-    }
-    
-    @GetMapping("/user")
-    @ResponseBody
-    public User getUserByEmail(@RequestParam String email) {
-        return userService.getUserByEmail(email);
-    }
-    
-    @PostMapping("/user/register")
-    @ResponseBody
-    public User createUser(@Valid @RequestBody CreateUserRequestBodyModel createUserRequestBody) {
-        System.out.println("in api:    "+ createUserRequestBody);
-        User user = new User();
-        Address address = new Address();
-        if(createUserRequestBody.getUid() != null) {
-            user.setUid(createUserRequestBody.getUid());
-        } else {
-            user.setPassword(createUserRequestBody.getPassword());
-        }
-        user.setEmail(createUserRequestBody.getEmail());
-        user.setScreenName(createUserRequestBody.getScreenName());
-        user.setNickName(createUserRequestBody.getNickName());
-        user.setCreditScore(0);
-        user.setVerified(false);
-        address.setStreet(createUserRequestBody.getStreet());
-        address.setCity(createUserRequestBody.getCity());
-        address.setState(createUserRequestBody.getState());
-        address.setZip(createUserRequestBody.getZip());
-        user.setAddress(address);
-        
-        return userService.createUser(user);
-    }
+	@GetMapping("/product/{storeId}")
+	@ResponseBody
+	public List<Product> getProducts(@PathVariable("storeId") long storeId) {
+		return productService.getProductsByStore(storeId);
+	}
+
+	@GetMapping("/user")
+	@ResponseBody
+	public User getUserByEmail(@RequestParam String email) {
+		return userService.getUserByEmail(email);
+	}
+
+	@PostMapping("/user/register")
+	@ResponseBody
+	public User createUser(@Valid @RequestBody CreateUserRequestBodyModel createUserRequestBody) {
+		System.out.println("in api:    " + createUserRequestBody);
+		User user = new User();
+		Address address = new Address();
+		if (createUserRequestBody.getUid() != null) {
+			user.setUid(createUserRequestBody.getUid());
+		} else {
+			user.setPassword(createUserRequestBody.getPassword());
+		}
+		user.setEmail(createUserRequestBody.getEmail());
+		user.setScreenName(createUserRequestBody.getScreenName());
+		user.setNickName(createUserRequestBody.getNickName());
+		user.setCreditScore(0);
+		user.setVerified(false);
+		address.setStreet(createUserRequestBody.getStreet());
+		address.setCity(createUserRequestBody.getCity());
+		address.setState(createUserRequestBody.getState());
+		address.setZip(createUserRequestBody.getZip());
+		user.setAddress(address);
+
+		return userService.createUser(user);
+	}
+
+	@PostMapping("/user/updateProfile")
+	@ResponseBody
+	public User updateUserProfile(@Valid @RequestBody UpdateUserProfileRequestBodyModel request) {
+		User user = new User();
+		Address address = new Address();
+		user.setEmail(request.getEmail());
+		user.setScreenName(request.getScreenName());
+		user.setNickName(request.getNickName());
+		address.setStreet(request.getStreet());
+		address.setCity(request.getCity());
+		address.setState(request.getState());
+		address.setZip(request.getZip());
+		user.setAddress(address);
+		return userService.createUser(user);
+	}
 }
