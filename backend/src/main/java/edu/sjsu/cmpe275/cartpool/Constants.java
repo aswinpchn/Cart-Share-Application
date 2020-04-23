9f3cd8ead90a6219ebe0a40396987e41b075504a
Vidita <viditavijaykumar.daga@sjsu.edu>
2020-04-22 18:52:16 -0700

    Code cleaning

--- backend/src/main/java/edu/sjsu/cmpe275/cartpool/Constants.java
+++ backend/src/main/java/edu/sjsu/cmpe275/cartpool/Constants.java
@@ -16,4 +16,5 @@ public final class Constants {
 	public static final String POOLER_ROLE = "pooler";
 	public static final String ADMIN_ROLE = "admin";
 	public static final String DOMAIN = "sjsu.edu";
+	public static final String BACKEND_URL = "http://localhost:3000";
 }
