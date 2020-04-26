a11671586f45bf95f9b305028235ec4fcb0c4134
Sruthi Duvvuri <sruthi.duvvuri@sjsu.edu>
2020-04-25 22:18:33 -0700

    Update Logout and Store apis backend
    Logout is added for UserDetailsForm
    Backend API's:
    Create Store api
    Edit Store api
    Delete Store api

--- backend/src/main/java/edu/sjsu/cmpe275/cartpool/service/StoreService.java
+++ backend/src/main/java/edu/sjsu/cmpe275/cartpool/service/StoreService.java
@@ -1,21 +1,55 @@
 package edu.sjsu.cmpe275.cartpool.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.server.ResponseStatusException;
 
 import edu.sjsu.cmpe275.cartpool.dto.Store;
 import edu.sjsu.cmpe275.cartpool.repository.StoreRepository;
 
 @Service
 public class StoreService {
-	
+
 	@Autowired
-	private StoreRepository storeRespository;
-	
+	private StoreRepository storeRepository;
+
 	public List<Store> getAllStores() {
-		return storeRespository.findAll();
+		return storeRepository.findAll();
+	}
+
+	@Transactional
+	public Store createStore(Store store) {
+		if (storeRepository.existsByName(store.getName())) {
+			Store existingStore = storeRepository.findStoreByName(store.getName());
+			return existingStore;
+		}
+		return storeRepository.save(store);
+	}
+
+	@Transactional
+	public Store updateStore(Store store) {
+		Store existingStore = storeRepository.findStoreByName(store.getName());
+		if (existingStore == null) {
+			throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Store not found");
+		}
+		existingStore.setAddress(store.getAddress());
+		Store response = storeRepository.save(existingStore);
+		return response;
+	}
+
+	@Transactional
+	public Store deleteStore(long storeId) {
+		Optional<Store> existingStore = storeRepository.findById(storeId);
+		if (!existingStore.isPresent()) {
+			throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Store not found");
+		}
+		storeRepository.deleteById(storeId);
+		return existingStore.get();
 	}
 
 }
--- frontend/src/components/AppEntry/Login.jsx
+++ frontend/src/components/AppEntry/Login.jsx
@@ -5,7 +5,6 @@ import "@fortawesome/fontawesome-free/css/all.min.css";
 import "bootstrap-css-only/css/bootstrap.min.css";
 import "mdbreact/dist/css/mdb.css";
 import classnames from "classnames";
-import Register from "./Register";
 import { Redirect } from "react-router-dom";
 import firebase from "firebase";
 import StyledFirebaseAuth from "react-firebaseui/StyledFirebaseAuth";
