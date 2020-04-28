b3507cf15ca5bb2d00b643295fac56e80b346207
Aswin Prasad <aswinpchn@gmail.com>
2020-04-28 02:50:40 -0700

    Pooler Navbar.

---
+++ frontend/src/components/Pooler/CreatePool.jsx
@@ -0,0 +1,22 @@
+import React, { Component } from "react";
+
+class CreatePool extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    }
+  }
+
+  render() {
+
+    return (
+      <div>
+        Create Pool
+      </div>
+    );
+  }
+}
+
+export default CreatePool;
