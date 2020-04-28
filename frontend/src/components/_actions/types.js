7927e922ed0d93413e44bc655db6711a2466a715
Sruthi Duvvuri <sruthi.duvvuri@sjsu.edu>
2020-04-27 17:13:33 -0700

    Adding Delete Store - Frontend
    Adding Delete Store - Frontend

--- frontend/src/components/_actions/types.js
+++ frontend/src/components/_actions/types.js
@@ -4,3 +4,4 @@ export const GET_ERRORS = "GET_ERRORS";
 export const STORE_LOADING = "STORE_LOADING";
 export const GET_STORES = "GET_STORES";
 export const UPDATE_STORE = "UPDATE_STORE";
+export const DELETE_STORE = "DELETE_STORE";
