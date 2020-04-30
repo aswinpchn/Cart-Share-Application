96b168bc706306b62ba805495ff4d453cd7452cd
SruthiD <55257437+duvsr01@users.noreply.github.com>
2020-04-30 09:50:26 -0700

    Update PoolRepository

--- backend/src/main/java/edu/sjsu/cmpe275/cartpool/repository/PoolRepository.java
+++ backend/src/main/java/edu/sjsu/cmpe275/cartpool/repository/PoolRepository.java
@@ -14,6 +14,5 @@ public interface PoolRepository extends JpaRepository<Pool, String> {
     public Pool findByName(String name);
     public Pool findByPoolId(String poolId);
     public Set<Pool> findByNameContainingOrNeighborhoodNameContainingOrZipContaining(String searchString1, String searchString2, String searchString3);
-    public Pool findByName(String name);
 
 }
