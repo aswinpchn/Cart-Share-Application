b91f91f136cb346c20009827380857e5d9a3b880
Vidita <viditavijaykumar.daga@sjsu.edu>
2020-05-03 14:38:00 -0700

    Code formatting

--- backend/src/main/java/edu/sjsu/cmpe275/cartpool/service/OrderService.java
+++ backend/src/main/java/edu/sjsu/cmpe275/cartpool/service/OrderService.java
@@ -13,56 +13,50 @@ import java.util.List;
 @Service
 public class OrderService {
 
-  @Autowired
-  private UserRepository userRepository;
+	@Autowired
+	private UserRepository userRepository;
 
-  @Autowired
-  private PoolRepository poolRepository;
+	@Autowired
+	private PoolRepository poolRepository;
 
-  @Autowired
-  private ProductRepository productRepository;
+	@Autowired
+	private ProductRepository productRepository;
 
-  @Autowired
-  private OrderRepository orderRepository;
+	@Autowired
+	private OrderRepository orderRepository;
 
-  @Autowired
-  private OrderDetailRepository orderDetailRepository;
+	public Boolean createDeferredOrder(DeferredOrderRequestModel deferredOrderRequestModel) {
+		System.out.println(deferredOrderRequestModel.toString());
 
-  public Boolean createDeferredOrder(DeferredOrderRequestModel deferredOrderRequestModel) {
-    System.out.println(deferredOrderRequestModel.toString());
+		if (!userRepository.findById(deferredOrderRequestModel.getPoolerId()).isPresent()) {
+			throw new ResponseStatusException(HttpStatus.NOT_FOUND, "No Pooler with this userId found");
+		}
 
-    if(!userRepository.findById(deferredOrderRequestModel.getPoolerId()).isPresent()) {
-      throw new ResponseStatusException(HttpStatus.NOT_FOUND, "No Pooler with this userId found");
-    }
+		if (poolRepository.findByPoolId(deferredOrderRequestModel.getPoolId()) == null) {
+			throw new ResponseStatusException(HttpStatus.NOT_FOUND, "No Pool with this PoolId found");
+		}
 
-    if(poolRepository.findByPoolId(deferredOrderRequestModel.getPoolId()) == null) {
-      throw new ResponseStatusException(HttpStatus.NOT_FOUND, "No Pool with this PoolId found");
-    }
+		if (deferredOrderRequestModel.getItems().size() == 0) {
+			throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "No items in this order");
+		}
 
-    if(deferredOrderRequestModel.getItems().size() == 0) {
-      throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "No items in this order");
-    }
+		Order order = new Order();
+		order.setPooler(userRepository.findById(deferredOrderRequestModel.getPoolerId()).get());
+		order.setPrice(deferredOrderRequestModel.getPrice());
+		order.setPoolId(deferredOrderRequestModel.getPoolId());
+		order.setStatus("Pending");
 
-    Order order = new Order();
-    order.setPooler(userRepository.findById(deferredOrderRequestModel.getPoolerId()).get());
-    order.setPrice(deferredOrderRequestModel.getPrice());
-    order.setPoolId(deferredOrderRequestModel.getPoolId());
-    order.setStatus("Pending");
-
-    List<OrderDetail> orderDetails = new ArrayList<>();
-    for(int i = 0; i < deferredOrderRequestModel.getItems().size(); i++) {
-      OrderDetail orderDetail = new OrderDetail();
-      orderDetail.setOrder(order);
-      orderDetail.setProduct(productRepository.getOne(deferredOrderRequestModel.getItems().get(i).getProductId()));
-      orderDetail.setPrice(deferredOrderRequestModel.getItems().get(i).getPrice());
-      orderDetail.setQuantity(deferredOrderRequestModel.getItems().get(i).getQuantity());
-
-      orderDetails.add(orderDetail);
-    }
-    order.setOrderDetails(orderDetails);
-
-    orderRepository.save(order);
-
-    return true;
-  }
+		List<OrderDetail> orderDetails = new ArrayList<>();
+		for (int i = 0; i < deferredOrderRequestModel.getItems().size(); i++) {
+			OrderDetail orderDetail = new OrderDetail();
+			orderDetail.setOrder(order);
+			orderDetail.setProduct(productRepository.getOne(deferredOrderRequestModel.getItems().get(i).getProductId()));
+			orderDetail.setPrice(deferredOrderRequestModel.getItems().get(i).getPrice());
+			orderDetail.setQuantity(deferredOrderRequestModel.getItems().get(i).getQuantity());
+			orderDetails.add(orderDetail);
+		}
+		order.setOrderDetails(orderDetails);
+		orderRepository.save(order);
+		return true;
+	}
 }
