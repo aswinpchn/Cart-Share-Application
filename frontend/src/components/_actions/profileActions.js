6dd2eefebf22d947836639b53e1c3dafdec66b7c
Sruthi Duvvuri <sruthi.duvvuri@sjsu.edu>
2020-05-04 12:43:09 -0700

    Updating verification check for ui
    Updating verification check for ui

--- frontend/src/components/_actions/profileActions.js
+++ frontend/src/components/_actions/profileActions.js
@@ -1,54 +1,33 @@
-// import axios from "axios";
-// import { GET_ERRORS } from "./types";
-// import { SET_PROFILE_DATA } from "./types";
-// import { UPDATE_PROFILE_DATA } from "./types";
-// import { rooturl } from "../utils/settings.js";
-// import { properties } from "../../properties";
-// const backendurl = properties.backendhost;
+import axios from "axios";
+import { GET_ERRORS } from "./types";
+import { SET_PROFILE_DATA } from "./types";
+import { UPDATE_PROFILE_DATA } from "./types";
+import { properties } from "../../properties";
+const backendurl = properties.backendhost;
 
-// //Get User Profile
-// export const getUserProfile = () => dispatch => {
-//   let email = localStorage.getItem("email");
+//Get User Profile
+export const getUserProfile = () => (dispatch) => {
+  let email = localStorage.getItem("email");
 
-//   console.log("Printing email_id " + email);
-//   axios
-//   .get(backendurl + "/user?email=" + email)
-//   .then((response) => {
-//       dispatch({
-//         type: SET_PROFILE_DATA,
-//         payload: response.data
-//       });
-//     })
-//     .catch(error => {
-//       dispatch({
-//         type: GET_ERRORS,
-//         payload: error.response.data
-//       });
-//     });
-// };
+  console.log("Printing email_id " + email);
+  axios
+    .get(backendurl + "/user?email=" + email)
+    .then((response) => {
+      dispatch({
+        type: SET_PROFILE_DATA,
+        payload: response.data,
+      });
+      localStorage.setItem("verified", response.data.verified);
+    })
+    .catch((error) => {
+      dispatch({
+        type: GET_ERRORS,
+        payload: error.response.data,
+      });
+    });
+};
 
-// let email = localStorage.getItem("email");
-//     axios
-//       .get(backendurl + "/user?email=" + email)
-//       .then((response) => {
-//         console.log("the response data is" + response.data);
-//         this.setState({
-//           screenName: response.data.screenName,
-//           nickName: response.data.nickName,
-//           email: response.data.email,
-//           poolId: response.data.poolId,
-//           contributionCredit: response.data.creditScore,
-//           street: response.data.address.street,
-//           city: response.data.address.city,
-//           state: response.data.address.state,
-//           zip: response.data.address.zip,
-//         });
-//       })
-//       .catch((error) => {
-//         console.log("Error while retreiving response");
-//       });
-
-// //Update User Profile
+//Update User Profile
 
 // export const updateUserProfile = data => dispatch => {
 //   axios
